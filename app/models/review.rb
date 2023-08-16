b18a14d11a572f5fc0c08168e2c297cdc35653b3
Nicoleta Grubi <msnicoleta@outlook.com>
2023-08-16 22:21:46 +0100

    completed to add the comments on all the files all works

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,19 @@
 class Review < ApplicationRecord
+  # Define the 'Review' model class that inherits from 'ApplicationRecord'.
+
   belongs_to :restaurant
+  # Establish a relationship: Each review belongs to a specific restaurant.
+  # This connection links reviews to their respective restaurants in the database.
+
   validates :rating, :content, presence: true
+  # Set validation rules: Both the rating and content must be present when saving a review.
 
   # validates :rating, numericality: true
   validates :rating, numericality: { only_integer: true }
+  # Validate the rating for numericality: It must only accept integer values (whole numbers).
+
   validates :rating, inclusion: { in: (0..5) }
+  # Restrict the rating attribute to a range of 0 to 5 (inclusive).
+  # This ensures that ratings fall within a typical 0 to 5 scale.
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   get 'reviews/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # This line creates a route for the 'create' action in the 'ReviewsController' using the GET HTTP method.
+  # It's usually not common to use GET for creating resources, so this might need to be changed to POST.
+
   root to: 'restaurants#index'
-  resources :restaurants, only: [:index, :show, :new, :create ] do
+  # This line sets the root route of your application to the 'index' action of the 'RestaurantsController'.
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    # This line defines a resource route for the 'restaurants' controller.
+    # It generates routes for listing, showing, creating, and viewing new restaurants.
+
     resources :reviews, only: [:create]
+    # This nested block defines a resource route for 'reviews' nested within 'restaurants'.
+    # It generates a route for creating reviews associated with a specific restaurant.
   end
 end
